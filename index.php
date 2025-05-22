ac13076856fa7420c5c79d362d45ad9829639d8d
AxelleBdd <axbaudinaud@gmail.com>
2025-05-22 15:51:03 +0200

    Step Two

--- index.php
+++ index.php
@@ -7,14 +7,22 @@
 </head>
 <body>
     <h1>
-        <?php
-        if ($_GET["first_name"] == '') {
-            echo 'Bonjour Anonyme.';
-
-        } else {
-            echo 'Bonjour ' . $_GET["first_name"] . '.';
-        }
-        ?>
     </h1>
+    
+    <form action="index.php" method="POST">
+        <p>Votre nom : <input type="text" name="first_name" /></p>
+        <p><input type="submit" value="OK"></p>
+    </form>
+
+    <?php
+        if ($_GET["first_name"]) {
+            echo 'Bonjour ' . $_GET["first_name"] . '.';
+        } elseif(isset($_POST['first_name'])){
+            echo 'Bonjour ,' .$_POST['first_name'];
+        } else {
+            echo 'Bonjour Anonyme.';
+        }
+        
+    ?>
 </body>
 </html>
