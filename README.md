79f6dc4a8dec23836910f5a854a465d872782465
AxelleBdd <axbaudinaud@gmail.com>
2025-05-23 13:32:03 +0200

    Readme and playing with unset()

---
+++ README.md
@@ -0,0 +1,16 @@
+># 👥 Individual exercise
+>
+>🏃 Sprint : 1 week alongside classes
+>
+
+># 📚 Used stack
+>
+>🌐 PHP
+>
+
+># 📑 Exercise guidlines
+>
+>🎲 Get used to PHP requests
+>
+>⌛ Follow steps to go as far as possible in the given time.
+>
--- index.php
+++ index.php
@@ -10,26 +10,29 @@
     </h1>
     
     <form action="index.php" method="POST">
-        <p>Votre nom : <input type="text" name="first_name" /></p>
+        <p>Votre nom : <input type="text" name="first_name"/></p>
         <p><input type="submit" value="OK"></p>
+    </form>
+    <form action="" method="post">
+        <p><input type="submit" value="Reset"></p>
     </form>
 
     <?php
-    session_start();
-    if ($_GET["first_name"]) {
-        echo 'Bonjour ' . $_GET["first_name"] . '.';
+        session_start();
+        if ($_GET["first_name"]) {
+            echo 'Bonjour ' . $_GET["first_name"] . '.'; //URL
 
-    } elseif($_SESSION["first_name"]) {
-        echo 'Bonjour ' .$_SESSION["first_name"]. '.';
+        } elseif($_SESSION["first_name"]) {
+            echo 'Bonjour ' .$_SESSION["first_name"]. '.'; //Affichage si donnée dans session
 
-    } elseif(isset($_POST['first_name'])) {
-        echo 'Bonjour ' .$_POST['first_name']. '.';
-        $_SESSION["first_name"] = $_POST['first_name'];
+        } elseif(isset($_POST['first_name'])) { //Récupération par POST de la donnée et enregistrement dans la session
+            echo 'Bonjour ' .$_POST['first_name']. '.';
+            $_SESSION["first_name"] = $_POST['first_name'];
 
-    } else {
-        echo 'Bonjour Anonyme.';
-    }
-        
+        } else {
+            echo 'Bonjour Anonyme.';
+        }
+        unset($_SESSION["first_name"]);  //Supprimer les données de la session (à lier au bouton Reset)
     ?>
 </body>
 </html>
